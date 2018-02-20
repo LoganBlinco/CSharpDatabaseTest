ac49314c1bf6c31876b89b76bc0305982df8799c
Logan Blinco (eLF) <BlincoLogan@gmail.com>
2018-02-20 22:06:44 +0000

    Got SQLLIte working with c#

---
+++ Program/Program/Program.cs
@@ -0,0 +1,74 @@
+﻿using Finisar.SQLite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Program
+{
+    class Program
+    {
+        //Tutorial video : https://www.youtube.com/watch?v=GLCtoHagrtE&t=323s
+
+        public static void Main(string[] args)
+        {
+            // We use these three SQLite objects:
+            SQLiteConnection sqlite_conn;
+            SQLiteCommand sqlite_cmd;
+            SQLiteDataReader sqlite_datareader;
+
+            // create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;");
+
+            // open the connection:
+            sqlite_conn.Open();
+
+            // create a new SQL command:
+            sqlite_cmd = sqlite_conn.CreateCommand();
+
+            // Let the SQLiteCommand object know our SQL-Query:
+            sqlite_cmd.CommandText = "CREATE TABLE test (id integer primary key, text varchar(100));";
+
+            // Now lets execute the SQL ;D
+            sqlite_cmd.ExecuteNonQuery();
+
+            // Lets insert something into our new table:
+            sqlite_cmd.CommandText = "INSERT INTO test (id, text) VALUES (1, 'Test Text 1');";
+
+            // And execute this again ;D
+            sqlite_cmd.ExecuteNonQuery();
+
+            // ...and inserting another line:
+            sqlite_cmd.CommandText = "INSERT INTO test (id, text) VALUES (2, 'Test Text 2');";
+
+            // And execute this again ;D
+            sqlite_cmd.ExecuteNonQuery();
+
+            // But how do we read something out of our table ?
+            // First lets build a SQL-Query again:
+            sqlite_cmd.CommandText = "SELECT * FROM test";
+
+            // Now the SQLiteCommand object can give us a DataReader-Object:
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+            // The SQLiteDataReader allows us to run through the result lines:
+            while (sqlite_datareader.Read()) // Read() returns true if there is still a result line to read
+            {
+                // Print out the content of the text field:
+                System.Console.WriteLine(sqlite_datareader["text"]);
+            }
+
+            // We are ready, now lets cleanup and close our connection:
+            sqlite_conn.Close();
+
+
+            //Your code goes here
+            Console.WriteLine("Hello, world!");
+        }
+
+    }
+}
